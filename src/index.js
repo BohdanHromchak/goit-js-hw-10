9b7780ec557fa2673b4ef0788bc5daa619bc45a0
BohdanHromchak <gromchakbogdan@gmail.com>
2023-01-08 01:19:01 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+// https://www.npmjs.com/package/throttle-debounce
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+import { fetchCountries } from './fetchCountries';
+
+const input = document.querySelector('#search-box');
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+
+    console.log(fetchCountries(input.value));
+
+}
