5d6046df6573c5c8078c984157e47d9fc569507f
BohdanHromchak <gromchakbogdan@gmail.com>
2023-01-08 14:44:08 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,63 @@
 import './css/styles.css';
-// https://www.npmjs.com/package/throttle-debounce
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-import { fetchCountries } from './fetchCountries';
-
 const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput() {
+  const country = input.value.trim();
+  fetchCountries(country)
+    .then(countries => {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
 
-    console.log(fetchCountries(input.value));
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (countries.length >= 2 && countries.length <= 10) {
+        createListMarkup(countries);
+      } else if (countries.length === 1) {
+        createCountryMarkup(countries);
+      }
+    })
+    .catch(() =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
+}
 
+function createListMarkup(countries) {
+  const listMarkup = countries
+    .map(
+      ({ flags, name }) =>
+        `<li class="country-list__item"><img src="${flags.svg}" alt="${name}" class="country-list__img"/>${name.official}</li>`
+    )
+    .join('');
+
+  countryList.insertAdjacentHTML('beforeend', listMarkup);
+}
+
+function createCountryMarkup(countries) {
+  const countryMarkup = countries
+    .map(
+      ({ flags, name, capital, population, languages }) =>
+        `<div class="country-info__title"><img src="${flags.svg}" alt="${
+          name.official
+        }" class="country-info__img"/>${name.official}</div>
+<ul class="country-info__list">
+  <p class="country-info__item"><b>Capital:</b>${capital}</p>
+  <p class="country-info__item"><b>Population:</b>${population}</p>
+  <p class="country-info__item"><b>Languages:</b>${Object.values(languages)}</p>
+</ul>`
+    )
+    .join('');
+
+  countryInfo.insertAdjacentHTML('beforeend', countryMarkup);
 }
